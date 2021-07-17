e290ed29d539658be6a9d8fde52c997829122aa3
dglee <slsldkf123@naver.com>
2021-07-17 23:16:13 +0900

    [JUnit] 자바 테스트 코드 작성 연습, JUnit5 의존성 수정

--- src/test/java/me/devhistory/FooTest.java
+++ src/test/java/me/devhistory/FooTest.java
@@ -1,42 +1,48 @@
 package me.devhistory;
 
-import org.junit.After;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.Assert.*;
-//junit 5 부터는 클래스가 public일 필요가 없다.
+import org.junit.jupiter.api.*;
+
+//junit 5 부터는 테스트 클래스, 테스트 메소드, 라이프사이클 메소드가 public일 필요가 없다. private 사용 불가능.
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) //Underscore를 빈 공백으로 치환
 class FooTest {
-
     @Test
-    void create() {
+    void create_new_foo() {
         Foo foo = new Foo();
-        assertNotNull(foo);
+        System.out.println("create");
     }
 
     @Test
-    void create1() {
+    @Disabled //해당 테스트를 실행하지 않겠다고 설정하는 애노테이션
+    void create_new_foo_again() {
         System.out.println("create1");
+    }
+
+    @Test
+    void create2() {
+        System.out.println("create2");
+    }
+
+    @Test
+    @DisplayName("\uD83D\uDC7E foo 만들기 \uD83D\uDC7E")
+    void create3() {
+        System.out.println("create3");
     }
 
     /**
      * 모든 테스트 실행전 단 한번 호출된다.
-     * static 메소드를 사용해서 구현해야한다.
-     * private X
-     * return type X
+     * static 메소드로 구현해야한다.
+     * return type void
      */
     @BeforeAll
     static void beforeAll() {
         System.out.println("beforeAll");
     }
+
     /**
      * 모든 테스트 실행된 이후 단 한번 호출된다.
-     * static 메소드를 사용해서 구현해야한다.
-     * private X
-     * return type X
+     * static 메소드로 구현해야한다.
+     * return type void
      */
     @AfterAll
     static void afterAll() {
@@ -44,20 +50,22 @@ class FooTest {
     }
 
     /**
-     * 모든 테스트를 실행할 떄,
+     * 모든 테스트를 실행할 때,
      * 각각의 테스트를 실행하기 이전에 한 번씩 호출
      */
     @BeforeEach
     void beforeEach() {
         System.out.println("beforeEach");
     }
+
     /**
-     * 모든 테스트를 실행할 떄,
-     * 각각의 테스트를 실행하기 이후에 한 번씩 호출
+     * 모든 테스트를 실행할 때,
+     * 각각의 테스트를 실행한 이후에 한 번씩 호출
      */
     @AfterEach
     void afterEach() {
         System.out.println("afterEach");
+        System.out.println();
     }
 
 }
